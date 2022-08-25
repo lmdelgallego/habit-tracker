3a4a0756b1f61cd71fc01d53e52b8ec983864ea9
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-08-25 08:05:24 -0500

    👨‍🔧 Advanced Authentication using an HTTP Header

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import requests
 
-pixela_endpoint = 'https://pixela.io/v1/users'
+USERNAME = 'lmdelgallego'
+TOKEN = "1q2w3e4r5t6"
+
+pixela_endpoint = 'https://pixe.la/v1/users'
 
 user_params = {
-  "token": "1q2w3e4r5t6",
-  "username": "lmdelgallego",
+  "token": TOKEN,
+  "username": USERNAME,
   "agreeTermsOfService": "yes",
   "notMinor": "yes",
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
 
+graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
+
+graph_config = {
+  "id": "graph-1",
+  "name": "Commits Graph",
+  "unit": "commit",
+  "type": "int",
+  "color": "kuro",
+}
+headers = {
+  "X-USER-TOKEN": TOKEN,
+}
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
+
