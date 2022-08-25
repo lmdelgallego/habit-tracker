9b09d8f6df5f934dacab273a58edcf7b2a746e45
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-08-25 08:15:34 -0500

    👨‍🔧  Add a pixel to the Habit tracker using a Post Request

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 
 USERNAME = 'lmdelgallego'
 TOKEN = "1q2w3e4r5t6"
+GRAPH_ID = "graph-1"
 
 pixela_endpoint = 'https://pixe.la/v1/users'
 
@@ -18,7 +19,7 @@ user_params = {
 graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
 
 graph_config = {
-  "id": "graph-1",
+  "id": GRAPH_ID,
   "name": "Commits Graph",
   "unit": "commit",
   "type": "int",
@@ -27,6 +28,15 @@ graph_config = {
 headers = {
   "X-USER-TOKEN": TOKEN,
 }
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}'
+pixel_params = {
+  "date": "20210825",
+  "quantity": "1",
+}
+
+response = requests.post(url=pixel_endpoint, json=pixel_params, headers=headers)
 print(response.text)
 
