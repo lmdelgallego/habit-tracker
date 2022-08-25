0aa4e6f49bc01033a7c9b9dce1ff0e7b22233972
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-08-25 08:38:22 -0500

    👨‍🔧   Delete and Update pixel, add .env variables

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv
 import requests
 from datetime import datetime
 
-USERNAME = 'lmdelgallego'
-TOKEN = "1q2w3e4r5t6"
+load_dotenv()
+
+USERNAME = os.environ.get('GRAPH_USERNAME')
+TOKEN = os.environ.get('GRAPH_TOKEN')
 GRAPH_ID = "graph-1"
 
 pixela_endpoint = 'https://pixe.la/v1/users'
@@ -17,6 +21,7 @@ user_params = {
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
+# CREATE GRAPH
 graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
 
 graph_config = {
@@ -32,15 +37,31 @@ headers = {
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
 
+# CREATE A PIXEL
 # today = datetime.now()
-today = datetime(2021, 8, 24)
+today = datetime(2021, 8, 24).strftime("%Y%m%d")
 
 pixel_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}'
 pixel_params = {
-  "date": today.strftime("%Y%m%d"),
+  "date": today,
   "quantity": "10",
 }
 
-response = requests.post(url=pixel_endpoint, json=pixel_params, headers=headers)
-print(response.text)
+# response = requests.post(url=pixel_endpoint, json=pixel_params, headers=headers)
+# print(response.text)
 
+# UPDATE PIXEL
+update_pixel_endpoint = f'{pixel_endpoint}/{today}'
+print(update_pixel_endpoint)
+update_pixel_params = {
+  "quantity": "20",
+}
+# response = requests.put(url=update_pixel_endpoint, json=update_pixel_params, headers=headers)
+# print(response.text)
+
+
+# DELETE PIXEL
+date_pixel = "20220824"
+delete_pixel_endpoint = f'{pixel_endpoint}/{date_pixel}'
+# response = requests.delete(url=delete_pixel_endpoint, headers=headers)
+# print(response.text)
